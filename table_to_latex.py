b59dafba42ce93ec6a29c48f6be62a16b1b9a9e7
agl2000 <andreugimenezlarred@gmail.com>
2025-01-17 13:49:16 +0100

    arreglar coses

--- table_to_latex.py
+++ table_to_latex.py
@@ -7,19 +7,65 @@ df = pd.read_csv('Metrics/metrics.csv')
 df['Number of Nodes'] = df['Number of Nodes'].round(0).astype(int)
 df['Average Node Degree'] = df['Average Node Degree'].round(2)
 df['Total Length (m)'] = df['Total Length (m)'].round(2)
-df['Average Street Length (m)'] = df['Average Edge Length (m)'].round(2)
-df['Average Betweenness Centrality (Nodes)'] = df['Betweenness Centrality (Nodes)'].round(4)
-df['Average Betweenness Centrality (Edges)'] = df['Betweenness Centrality (Edges)'].round(4)
-df['Orientation Entropy'] = df['Orientation Entropy'].round(2)
+df['Average Street Length (m)'] = df['Average Street Length (m)'].round(2)
+df['Average Betweenness Centrality (Nodes)'] = df['Average Betweenness Centrality (Nodes)'].round(4)
+df['Average Betweenness Centrality (Edges)'] = df['Average Betweenness Centrality (Edges)'].round(4)
+df['Normalized Measure of Orientation-Order'] = df['Normalized Measure of Orientation-Order'].round(4)
 df['Proportion of Dead-ends'] = df['Proportion of Dead-ends'].round(4)
 df['Proportion of k=4 Intersections'] = df['Proportion of k=4 Intersections'].round(4)
 df['Detour Index'] = df['Detour Index'].round(2)
 
-# Convert the DataFrame to a LaTeX table 
-latex_table = df.to_latex(index=False)
+
+# Rename columns to single characters
+df.columns = ['City', 'N', 'D', 'L', 'S', 'B_N', 'B_E', 'O','phi', 'P_D', 'P_4', 'I']
+
+
+# Convert the DataFrame to a LaTeX table with the specified format
+latex_table = r"""
+\begin{tabular}{l@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r}
+\toprule
+City & N & D & L & S & B_N\\
+\midrule
+"""
+
+for index, row in df.iterrows():
+    latex_table += f"  {row['City']} & {row['N']} & {row['D']} & {row['L']} & {row['S']} & {row['B_N']}\\\\ \n"
+
+latex_table += r"""
+\bottomrule
+\end{tabular}
+"""
+
 
 # Save the LaTeX table to a file using utf8 encoding
-with open('Metrics/metrics_table.tex', 'w', encoding='utf8') as f:
+with open('metrics_table1.tex', 'w', encoding='utf8') as f:
     f.write(latex_table)
 
-print("LaTeX table saved to 'metrics_table.tex'")
+print("First LaTeX table saved to 'metrics_table1.tex'")
+
+
+
+
+# Convert the DataFrame to a LaTeX table with the specified format
+latex_table = r"""
+\begin{tabular}{l@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r@{\hskip 2em}r}
+\toprule
+City & B_E & \phi & P_D & P_4 & I \\
+\midrule
+"""
+
+for index, row in df.iterrows():
+    latex_table += f"  {row['City']} & {row['B_E']} & {row['phi']} & {row['P_D']} & {row['P_4']} & {row['I']} \\\\ \n"
+
+latex_table += r"""
+\bottomrule
+\end{tabular}
+
+"""
+
+
+# Save the LaTeX table to a file using utf8 encoding
+with open('metrics_table2.tex', 'w', encoding='utf8') as f:
+    f.write(latex_table)
+
+print("Second LaTeX table saved to 'metrics_table2.tex'")
